ad849a3bd8b7a5ae758e55c5f4aee7bf966a376b
chamara8901 <chamara@grrow.jp>
2022-11-09 19:38:45 +0900

    create a README.md

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -6,7 +6,7 @@ module.exports = {
     themeConfig: {
         nav:[
             {text:'Home', link:'/'},
-            {text:'Documents', link:'/docs/'}
+            {text:'Documents', link:'/documents/'}
         ],
         sidebar:{
             '/docs/':[
